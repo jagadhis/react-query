b08ba41409481e19441a6f8edf57125acfc5cc2b
jagadhis <jwaran78@gmail.com>
2022-08-27 16:30:01 +0530

    Using Timer

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import { useQuery } from 'react-query'
 import axios from 'axios'
 
 export default function App() {
-  const queryInfo = useQuery('pokemon', () =>
-    axios
+  const queryInfo = useQuery('pokemon', async() =>{
+    await new Promise(resolve => setTimeout(resolve,5000))
+    return axios
       .get('https://rickandmortyapi.com/api/character')
       .then(res => res.data.results)
-  )
+  })
   console.log(queryInfo)
   return queryInfo.isLoading ? (
     "Loading ..." 
-  ):(
+  ): queryInfo.isError?(
+    queryInfo.error.message):(
     <div>
 
       {queryInfo.data?.map(data => {
