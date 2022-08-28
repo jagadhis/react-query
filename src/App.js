9f62a43c50c3592a8c0f3feafe7c9a598d7c59c2
jagadhis <jwaran78@gmail.com>
2022-08-28 06:05:04 +0530

    Commented The Old Code

--- src/App.js
+++ src/App.js
@@ -4,79 +4,89 @@ import { ReactQueryDevtools } from 'react-query/devtools';
 import axios from 'axios'
 
 
-function useRick(){
-  return useQuery("Rick",async()=>{
+
+//Commented For Trying New Concepts
+
+// function useRick(){
+//   return useQuery("Rick",async()=>{
+//     await new Promise(resolve => setTimeout(resolve,1000))
+//     return axios
+//       .get('https://rickandmortyapi.com/api/character')
+//       .then(res => res.data.results)
+  
+//   })
+// }
+
+// function Count(){
+//   const queryInfo = useRick();
+  
+//   return <h3>You are looking at {queryInfo.data?.length}</h3>
+// }
+// function Rick() {
+// const queryInfo = useRick();
+//   return queryInfo.isLoading ? (
+//     "Loading ..." 
+//   ): queryInfo.isError?(
+//     queryInfo.error.message):(
+//     <div>
+
+//       {queryInfo.data?.map(data => {
+//         return (
+//           <div>
+//             <p>{data.id}</p>
+//             <p>{data.origin.name}</p>
+//             <p>{data.gender}</p>
+//             <p>{data.species}</p>
+//             <p>{data.image}</p>
+//             <p>{data.name}</p>
+//             <p>{data.status}</p>
+//           </div>
+//         )
+//       })}
+//        <br />
+//       {queryInfo.isFetching ? 'Updating...' : null}
+//     </div>
+//   )
+// }
+
+// function Monty() {
+//   const queryInfo = useQuery('Monty', async () => {
+//     await new Promise(resolve => setTimeout(resolve, 1000))
+//     return axios
+//       .get('https://rickandmortyapi.com/api/character/?page=20')
+//       .then(res => res.data.results)
+//   })
+
+//   return queryInfo.isLoading ? (
+//     'Loading...'
+//   ) : queryInfo.isError ? (
+//     queryInfo.error.message
+//   ) : (
+//     <div>
+//       {queryInfo.data.map(data => {
+//         return( <div>
+//            <p>{data.id}</p>
+//             <p>{data.origin.name}</p>
+//             <p>{data.gender}</p>
+//             <p>{data.species}</p>
+//             <p>{data.image}</p>
+//             <p>{data.name}</p>
+//             <p>{data.status}</p>
+//         </div> )
+//       })}
+//       <br />
+//       {queryInfo.isFetching ? 'Updating...' : null}
+//     </div>
+//   )
+// }
+
+
+function RickSearch({Rick}){
+  const queryInfo = useQuery(Rick,async()=>{
     await new Promise(resolve => setTimeout(resolve,1000))
-    return axios
-      .get('https://rickandmortyapi.com/api/character')
-      .then(res => res.data.results)
-  
+    return axios.get(`https://rickandmortyapi.com/api/character`).then(res=>res.data)
   })
 }
-
-function Count(){
-  const queryInfo = useRick();
-  
-  return <h3>You are looking at {queryInfo.data?.length}</h3>
-}
-function Rick() {
-const queryInfo = useRick();
-  return queryInfo.isLoading ? (
-    "Loading ..." 
-  ): queryInfo.isError?(
-    queryInfo.error.message):(
-    <div>
-
-      {queryInfo.data?.map(data => {
-        return (
-          <div>
-            <p>{data.id}</p>
-            <p>{data.origin.name}</p>
-            <p>{data.gender}</p>
-            <p>{data.species}</p>
-            <p>{data.image}</p>
-            <p>{data.name}</p>
-            <p>{data.status}</p>
-          </div>
-        )
-      })}
-       <br />
-      {queryInfo.isFetching ? 'Updating...' : null}
-    </div>
-  )
-}
-
-function Monty() {
-  const queryInfo = useQuery('Monty', async () => {
-    await new Promise(resolve => setTimeout(resolve, 1000))
-    return axios
-      .get('https://rickandmortyapi.com/api/character/?page=20')
-      .then(res => res.data.results)
-  })
-
-  return queryInfo.isLoading ? (
-    'Loading...'
-  ) : queryInfo.isError ? (
-    queryInfo.error.message
-  ) : (
-    <div>
-      {queryInfo.data.map(data => {
-        return( <div>
-           <p>{data.id}</p>
-            <p>{data.origin.name}</p>
-            <p>{data.gender}</p>
-            <p>{data.species}</p>
-            <p>{data.image}</p>
-            <p>{data.name}</p>
-            <p>{data.status}</p>
-        </div> )
-      })}
-      <br />
-      {queryInfo.isFetching ? 'Updating...' : null}
-    </div>
-  )
-}
-
 
 
 export default function App(){
