f8a1055d8a862d7f5dc4b2b11f853d966dca4fa5
jagadhis <jwaran78@gmail.com>
2022-08-31 16:43:22 +0530

    Related Resources

--- src/App.js
+++ src/App.js
@@ -157,13 +157,80 @@ import initialData from './initialData';
 
 // }
 
-export default function App() {
+
+function Posts({ setPostId }) {
+  const postsQuery = useQuery('posts', async () => {
+    await new Promise(resolve => setTimeout(resolve, 1000))
+    return axios
+      .get('https://jsonplaceholder.typicode.com/posts')
+      .then(res => res.data)
+  })
 
   return (
     <div>
+      <h1>Posts {postsQuery.isFetching ? '...' : null}</h1>
+      <div>
+        {postsQuery.isLoading ? (
+          'Loading posts...'
+        ) : (
+          <ul>
+            {postsQuery.data.map(post => {
+              return (
+                <li key={post.id}>
+                  <a onClick={() => setPostId(post.id)} href="#">
+                    {post.title}
+                  </a>
+                </li>
+              )
+            })}
+          </ul>
+        )}
+      </div>
+    </div>
+  )
+}
 
-      <MyPosts />
+function Post({ postId, setPostId }) {
+  const postQuery = useQuery(['post', postId], async () => {
+    await new Promise(resolve => setTimeout(resolve, 1000))
+    return axios
+      .get(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+      .then(res => res.data)
+  })
 
+  return (
+    <div>
+      <a onClick={() => setPostId(-1)} href="#">
+        Back
+      </a>
+      <br />
+      <br />
+      {postQuery.isLoading ? (
+        'Loading...'
+      ) : (
+        <>
+          {postQuery.data.title}
+          <br />
+          <br />
+          {postQuery.isFetching ? 'Updating...' : null}
+        </>
+      )}
+    </div>
+  )
+  //
+}
+
+export default function App() {
+
+  const [postId, setPostId] = React.useState(-1)
+
+  return (
+    <div>
+      {postId > -1 ? (
+        <Post postId={postId} setPostId={setPostId} />
+      ) : (
+        <Posts setPostId={setPostId} />
+      )}
       <ReactQueryDevtools />
     </div>
   )
