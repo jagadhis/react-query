af3cbadbbf1ba7a4a4b114ac6dcf30e6fbe60499
jagadhis <jwaran78@gmail.com>
2022-09-03 04:45:00 +0530

    Export Function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import App from './Pages/_app';
 import { useQuery } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools';
 import axios from 'axios'
@@ -165,71 +166,71 @@ import {
 // }
 
 
-function Posts() {
-  const postsQuery = useQuery(
-    'posts',
-    async () => {
-      await new Promise(resolve => setTimeout(resolve, 1000))
-      return axios
-        .get('https://jsonplaceholder.typicode.com/posts')
-        .then(res => res.data)
-    },
-    {
-      cacheTime: 10000,
-    }
-  )
+// function Posts() {
+//   const postsQuery = useQuery(
+//     'posts',
+//     async () => {
+//       await new Promise(resolve => setTimeout(resolve, 1000))
+//       return axios
+//         .get('https://jsonplaceholder.typicode.com/posts')
+//         .then(res => res.data)
+//     },
+//     {
+//       cacheTime: 10000,
+//     }
+//   )
 
-  return (
-    <div>
-      <h1>Posts {postsQuery.isFetching ? '...' : null}</h1>
-      <div>
-        {postsQuery.isLoading ? (
-          'Loading posts...'
-        ) : (
-          <ul>
-            {postsQuery.data.map(post => {
-              return (
-                <li key={post.id}>
-                  <Link to={`/${post.id}`}>{post.title}</Link>
-                </li>
-              )
-            })}
-          </ul>
-        )}
-      </div>
-    </div>
-  )
-}
+//   return (
+//     <div>
+//       <h1>Posts {postsQuery.isFetching ? '...' : null}</h1>
+//       <div>
+//         {postsQuery.isLoading ? (
+//           'Loading posts...'
+//         ) : (
+//           <ul>
+//             {postsQuery.data.map(post => {
+//               return (
+//                 <li key={post.id}>
+//                   <Link to={`/${post.id}`}>{post.title}</Link>
+//                 </li>
+//               )
+//             })}
+//           </ul>
+//         )}
+//       </div>
+//     </div>
+//   )
+// }
 
-function Post() {
-  const { postId } = useParams()
+// function Post() {
+//   const { postId } = useParams()
 
-  const postQuery = useQuery(['post', postId], async () => {
-    await new Promise(resolve => setTimeout(resolve, 1000))
-    return axios
-      .get(`https://jsonplaceholder.typicode.com/posts/${postId}`)
-      .then(res => res.data)
-  })
+//   const postQuery = useQuery(['post', postId], async () => {
+//     await new Promise(resolve => setTimeout(resolve, 1000))
+//     return axios
+//       .get(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+//       .then(res => res.data)
+//   })
 
-  return (
-    <div>
-      <Link to="/">Back</Link>
-      <br />
-      <br />
-      {postQuery.isLoading ? (
-        'Loading...'
-      ) : (
-        <>
-          {postQuery.data.title}
-          <br />
-          <br />
-          {postQuery.isFetching ? 'Updating...' : null}
-        </>
-      )}
-    </div>
-  )
-  //
-}
+//   return (
+//     <div>
+//       <Link to="/">Back</Link>
+//       <br />
+//       <br />
+//       {postQuery.isLoading ? (
+//         'Loading...'
+//       ) : (
+//         <>
+//           {postQuery.data.title}
+//           <br />
+//           <br />
+//           {postQuery.isFetching ? 'Updating...' : null}
+//         </>
+//       )}
+//     </div>
+//   )
+//   //
+// }
 
 
 export default function App() {
@@ -237,17 +238,18 @@ export default function App() {
 
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/:postId" element={<Post />}>
+    <div><App /></div>
+    // <Router>
+    //   <Routes>
+    //     <Route path="/:postId" element={<Post />}>
 
-        </Route>
-        <Route path="/" element={<Posts />}>
+    //     </Route>
+    //     <Route path="/" element={<Posts />}>
 
-        </Route>
-      </Routes>
-      <ReactQueryDevtools />
-    </Router>
+    //     </Route>
+    //   </Routes>
+    //   <ReactQueryDevtools />
+    // </Router>
   )
 }
 
